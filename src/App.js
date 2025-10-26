d8ba3bb8b71a14b9bc7696f15b99b4ff727d6859
vedadhunanrapu <24211a6725@bvrit.ac.in>
2025-10-26 19:35:47 +0530

    Trigger redeploy for Vercel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+// Trigger redeploy
 
 export default function App() {
   const [articles, setArticles] = useState([]);
